7ebde3f8827b376fbbca2a678b94079e87742675
Eric Nguyen <enguyen807@gmail.com>
2023-06-06 03:37:36 +0300

    Add base canvas

---
+++ src/components/BaseComponent/BaseCanvas.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineProps<{
+  width: number;
+  height: number
+}>()
+</script>
+
+<template>
+    <canvas id="gameCanvas" :width="width" :height="height"></canvas>
+</template>
